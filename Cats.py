286592abb49d8c02588e4467249c2231193e39c8
Докс <nox23061992@gmail.com>
2024-10-15 20:54:58 +0300

    создана функция для получения изображения с котиком

--- Cats.py
+++ Cats.py
@@ -4,8 +4,18 @@ import requests
 from io import BytesIO
 
 
+def load_image():
+    try:
+        response = requests.get(url) #делаем запрос из ссылки
+        response.raise_for_status() #строка нужна для обработки исключений, если будет какая то ошибка
+        image_data = BytesIO(response.content) #в переменную положим обработанное изображение. контент ответа будет преобразована с помощью BytesIO
+        img = Image.open(image_data) #открываем картинку с помощью библиотеки Pillow
+        return ImageTk.PhotoImage(img) #функция возвращает эту картинку
+    except Exception as e:
+        print(f'Произошла ошибка {e}')
+        return None
 
-window = Tk()
+
 window.title('Cats')
 window.geometry('600x480')
 
